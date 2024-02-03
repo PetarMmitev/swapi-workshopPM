bd903af8635e3a77f6b2881b052c90a971031cda
PetarMmitev <petarmitev67@gmail.com>
2024-02-03 11:30:21 +0100

    workshop 2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("its working");
 const containerEl = document.querySelector(".container");
 
 const STARWARS_URL = "https://swapi.dev/api/people";
+const STARWARS_SHIP_URL = "https://swapi.dev/api/starships";
 
 function fetchCharacters() {
   fetch(STARWARS_URL)
@@ -15,6 +16,18 @@ function fetchCharacters() {
 }
 
 fetchCharacters();
+
+function fetchShips() {
+  fetch(STARWARS_SHIP_URL)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  renderShipTable(containerEl, data.results);
+}
+fetchShips();
 
 function renderCharacterTable(containerEl, characters) {
   let characterHTML = "";
@@ -41,6 +54,35 @@ function renderCharacterTable(containerEl, characters) {
      </thead>
      <tbody>
        ${characterHTML}
+     </tbody>
+   </table>
+`;
+}
+function renderShipTable(containerEl, starships) {
+  let characterHTML = "";
+  for (let starship of starships) {
+    characterHTML += `
+    <tr>
+    <td>${starship.name}</td>
+    <td>${starship.model}</td>
+    <td>${starship.manufacturer}</td>
+    <td>${starship.cost}</td>
+    <td>${starship.class}</td>
+  </tr>`;
+  }
+  containerEl.innerHTML = `
+  <table>
+     <thead>
+       <tr>
+         <th>Name</th>
+         <th>Height</th>
+         <th>Mass</th>
+         <th>Gender</th>
+         <th>Birth year</th>
+       </tr>
+     </thead>
+     <tbody>
+       ${starshipHTML}
      </tbody>
    </table>
 `;
